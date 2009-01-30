e90bdcf2b1457e2a992adfba7a39d647b1a29ad5
Graeme Mathieson <mathie@woss.name>
2009-01-30 10:16:47 +0000

    Specify the initializer for the cipher.

--- lib/solitaire_cipher.rb
+++ lib/solitaire_cipher.rb
@@ -1,3 +1,8 @@
+require 'active_support'
+
 class SolitaireCipher
-  
+  def initialize(key_stream)
+    raise ArgumentError.new("invalid argument 1, which should be a key stream") if key_stream.blank?
+    @key_stream = key_stream
+  end
 end
--- spec/solitaire_cipher_spec.rb
+++ spec/solitaire_cipher_spec.rb
@@ -1,5 +1,19 @@
 require File.join(File.dirname(__FILE__), 'spec_helper')
 
-describe SolitaireCipher do
-  
+describe SolitaireCipher, "creation" do
+  it "should allow us to create a new instance with a key stream as a string" do
+    lambda {
+      SolitaireCipher.new("KEYS TREA MONE")
+    }.should_not raise_error
+  end
+
+  it "should require a key steam to be specified" do
+    lambda {
+      SolitaireCipher.new()
+    }.should raise_error(ArgumentError)
+
+    lambda {
+      SolitaireCipher.new(nil)
+    }.should raise_error(ArgumentError)
+  end
 end
