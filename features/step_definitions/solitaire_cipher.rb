2c1bb6b071ba6b3ff26abe71cd04dcd82c395b47
Graeme Mathieson <mathie@woss.name>
2009-01-30 09:08:55 +0000

    Implement the step definitions in terms of the imaginary interface.  Steps fail, so far.

--- features/step_definitions/solitaire_cipher.rb
+++ features/step_definitions/solitaire_cipher.rb
@@ -1,14 +1,19 @@
 Given /^the key stream "(.*)"$/ do |key_stream|
+  @solitaire = SolitaireCipher.new(key_stream)
 end
 
 When /^I decode the string "(.*)"$/ do |encoded_string|
+  @decoded_string = @solitaire.decode(encoded_string)
 end
 
 Then /^it should produce the plain text "(.*)"$/ do |plain_text_string|
+  @decoded_string.should == plain_text_string
 end
 
 When /^I encode the string "(.*)"$/ do |plain_text_string|
+  @encoded_string = @solitaire.encode(plain_text_string)
 end
 
 Then /^it should produce the cipher text "(.*)"$/ do |encoded_string|
+  @encoded_string.should == encoded_string
 end
