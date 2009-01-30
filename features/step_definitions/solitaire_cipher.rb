201b42336f34a13b1341a63f199f771a3b2a4f6c
Graeme Mathieson <mathie@woss.name>
2009-01-30 08:53:24 +0000

    Skeleton implementations of the steps.

---
+++ features/step_definitions/solitaire_cipher.rb
@@ -0,0 +1,14 @@
+Given /^the key stream "DWJXH YRFDG TMSHP UURXJ"$/ do
+end
+
+When /^I decode the string "GLNCQ MJAFF FVOMB JIYCB"$/ do
+end
+
+Then /^it should produce the plain text "CODEI NRUBY LIVEL ONGER"$/ do
+end
+
+When /^I encode the string "Code in Ruby, live longer!"$/ do
+end
+
+Then /^it should produce the cipher text "GLNCQ MJAFF FVOMB JIYCB"$/ do
+end
